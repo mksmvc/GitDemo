6e00570aafdd4c87fb53aaf4fe5b2ac2252420bb
Milica Maksimovic <bubnjevich@gmail.com>
2021-02-16 15:24:46 +0400

    this is commit from GitDemo

---
+++ src/test/java/Academy/E2EProject/GitDemoAddition.java
@@ -0,0 +1,24 @@
+package Academy.E2EProject;
+
+
+import java.io.IOException;
+import org.testng.annotations.Test;
+import resources.base;
+
+public class GitDemoAddition extends base{
+
+	
+	@Test
+	public void demoTest1() throws IOException {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+	
+	@Test
+	public void demoTest2() throws IOException {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+}
