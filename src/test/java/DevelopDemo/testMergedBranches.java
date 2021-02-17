07b902f55745542176fee0f2fa8c3a5ec0b0c500
Milica Maksimovic <bubnjevich@gmail.com>
2021-02-17 12:11:48 +0400

    merged branches - final commit by GitX

---
+++ src/test/java/DevelopDemo/testMergedBranches.java
@@ -0,0 +1,13 @@
+package DevelopDemo;
+
+import java.io.IOException;
+
+public class testMergedBranches {
+	public void testMerged() throws IOException {
+		System.out.println("merged 1");
+	}
+	
+	public void testMergedBranches() throws IOException {
+		System.out.println("merged 2");
+	}
+}
