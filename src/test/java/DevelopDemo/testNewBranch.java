c21b278378f79e160aea4a2e6b6d17847d2cdc1c
Milica Maksimovic <bubnjevich@gmail.com>
2021-02-17 11:37:29 +0400

    new branch development - GitDemo

---
+++ src/test/java/DevelopDemo/testNewBranch.java
@@ -0,0 +1,10 @@
+package DevelopDemo;
+
+import java.io.IOException;
+
+public class testNewBranch {
+	public void demoTestBranch() throws IOException {
+		System.out.println("new branch is created");
+		
+	}
+}
