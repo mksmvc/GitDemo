c17fc9c5e728bc5cc063697660c4d3b3a645f1a2
Milica Maksimovic <bubnjevich@gmail.com>
2021-02-17 11:51:28 +0400

    commit to a new branch - GitX

--- src/test/java/DevelopDemo/testNewBranch.java
+++ src/test/java/DevelopDemo/testNewBranch.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 public class testNewBranch {
 	public void demoTestBranch() throws IOException {
 		System.out.println("new branch is created");
-		
+	}
+	
+	public void demoTestBranchX() throws IOException {
+		System.out.println("addition from GitX");
 	}
 }
